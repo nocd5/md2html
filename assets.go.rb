cc2c81483a3c937a05737993cfffd739d16474c2
nocd5 <nocd5rd@gmail.com>
2016-04-01 08:52:43 +0900

    Update assets.go.rb
    - cosmetic changes of assets.go.rb
      - checked by rubcop
    - improve formatting of assets.go generated by assets.go.rb
      - output in hex format
      - wrap at 16 byte

--- assets.go.rb
+++ assets.go.rb
@@ -1,22 +1,38 @@
 #!/usr/bin/env ruby
+
 parent = File.expand_path(File.dirname(__FILE__))
 
-js = ""
-Dir::glob(parent + '/assets/*.js').each { |f|
-    js += '<script type="text/javascript">' + "\n"
-    js += File.read(f, :encoding => Encoding::UTF_8) + "\n"
-    js += '</script>' + "\n"
-}
-js_bytes = js.unpack('C*').join(', ')
+js = ''
+Dir.glob(parent + '/assets/*.js').each do |f|
+  js += '<script type="text/javascript">' + "\n"
+  js += File.read(f, encoding: Encoding::UTF_8) + "\n"
+  js += '</script>' + "\n"
+end
+js_bytes = js.unpack('C*')
+             .map { |b| format('0x%02X', b) }
+             .join(', ')
+             .gsub(/(?:0x\w{2},\s*){16}/, "\\0\n")
+             .gsub(/\s+\n/, "\n")
 
-css = ""
-Dir::glob(parent + '/assets/*.css').each { |f|
-    css += '<style type="text/css">' + "\n"
-    css += File.read(f, :encoding => Encoding::UTF_8) + "\n"
-    css += '</style>'  + "\n"
-}
-css_bytes = css.unpack('C*').join(', ')
+css = ''
+Dir.glob(parent + '/assets/*.css').each do |f|
+  css += '<style type="text/css">' + "\n"
+  css += File.read(f, encoding: Encoding::UTF_8) + "\n"
+  css += '</style>' + "\n"
+end
+css_bytes = css.unpack('C*')
+               .map { |b| format('0x%02X', b) }
+               .join(', ')
+               .gsub(/((?:0x\w{2},\s*){16})/, "\\1\n")
+               .gsub(/\s+\n/, "\n")
 
-File.open(parent + '/assets.go', "wb") { |f|
-    f.puts "package main\n\nvar js_bytes = [...]byte{#{js_bytes}}\nvar css_bytes = [...]byte{#{css_bytes}}"
+File.open(parent + '/assets.go', 'wb') do |f|
+  f.puts "package main
+
+var js_bytes = [...]byte{
+	#{js_bytes.gsub(/\n/, "\n\t")},
 }
+var css_bytes = [...]byte{
+	#{css_bytes.gsub(/\n/, "\n\t")},
+}"
+end
