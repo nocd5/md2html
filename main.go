22d2dfbd9ce69708b27251e1f97f7deae2194e39
nocd5 <nocd5rd@gmail.com>
2016-04-01 21:27:30 +0900

    Add option -e/--embed
    embed images that is located local storage as base64

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"bufio"
+	"encoding/base64"
 	"fmt"
 	"github.com/jessevdk/go-flags"
 	"github.com/russross/blackfriday"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
 )
 
 type Options struct {
 	InputFile  string `long:"input" short:"i" description:"input Markdown"`
 	OutputFile string `long:"output" short:"o" description:"output HTML"`
+	EmbedImage bool   `long:"embed" short:"e" description:"embed image by base64 encoding"`
 }
 
 const (
@@ -25,6 +29,7 @@ const (
 <div class="markdown-body">%s</div>
 </body>
 </html>`
+
 	extensions = blackfriday.EXTENSION_NO_INTRA_EMPHASIS |
 		blackfriday.EXTENSION_TABLES |
 		blackfriday.EXTENSION_FENCED_CODE |
@@ -53,14 +58,24 @@ func main() {
 	}
 	defer fi.Close()
 
-	scanner := bufio.NewScanner(fi)
-	md := ""
-	for scanner.Scan() {
-		md = md + scanner.Text() + "\n"
+	md, err := ioutil.ReadAll(fi)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
+
 	js := string(js_bytes[:len(js_bytes)])
 	css := string(css_bytes[:len(css_bytes)])
-	output := blackfriday.MarkdownCommon([]byte(md))
+	html := string(blackfriday.MarkdownCommon([]byte(md)))
+
+	if opts.EmbedImage {
+		html, err = embedImage(html)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, "Embedding image is failed.")
+			os.Exit(1)
+		}
+	}
 
 	output_name := opts.InputFile + ".html"
 	if len(opts.OutputFile) > 0 {
@@ -74,5 +89,53 @@ func main() {
 	}
 	defer fo.Close()
 
-	fmt.Fprintf(fo, template, opts.InputFile, js+"\n"+css, string(output))
+	fmt.Fprintf(fo, template, opts.InputFile, js+"\n"+css, html)
+}
+
+func embedImage(src string) (string, error) {
+	re_find, err := regexp.Compile(`(<img[\S\s]+?src=")([\S\s]+?)("[\S\s]+?/>)`)
+	if err != nil {
+		return src, err
+	}
+	img_tags := re_find.FindAllString(src, -1)
+
+	dest := src
+	dir := filepath.Dir(opts.InputFile)
+	for _, t := range img_tags {
+		img_src := re_find.ReplaceAllString(t, "$2")
+		if filepath.IsAbs(img_src) {
+			img_src, _ = filepath.Rel(dir, img_src)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+		}
+
+		datatype := "imgae"
+		ext := filepath.Ext(img_src)
+		if len(ext) > 0 {
+			datatype = "image/" + ext[1:]
+		}
+
+		f, err := os.Open(img_src)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		defer f.Close()
+
+		d, err := ioutil.ReadAll(f)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		b64img := base64.StdEncoding.EncodeToString(d)
+		re_replace, err := regexp.Compile(`(<img[\S\s]+?src=")` + regexp.QuoteMeta(img_src) + `("[\S\s]+?/>)`)
+		if err != nil {
+			return src, err
+		}
+		dest = re_replace.ReplaceAllString(dest, "${1}data:"+datatype+";base64,"+b64img+"${2}")
+	}
+	return dest, nil
 }
