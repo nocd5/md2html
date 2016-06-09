4aa7b1165668d58f887920c6620baaaad3dd36b8
nocd5 <nocd5rd@gmail.com>
2016-06-09 23:55:55 +0900

    Tweak javascript

--- src/zzz-post.js
+++ src/zzz-post.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     // if without option "--toc/-t" do nothing
     if (document.getElementById('markdown-toc') == null) return;
 
@@ -13,40 +13,37 @@ document.addEventListener('DOMContentLoaded', function(){
     scroll = document.querySelector('#markdown-toc > .scroll');
 
     // generate TOC
-    $(scroll).toc({'selectors': 'h1,h2,h3,h4', 'highlightOffset': 0});
+    $(scroll).toc({ selectors: 'h1,h2,h3,h4', highlightOffset: 0 });
     // show scrollbar on left side
     toc.style.direction = 'rtl';
     scroll.style.direction = 'ltr';
 
     // take clearance for TOC area
-    window.onload = window.onresize = function(){
+    window.onload = window.onresize = function() {
         body.style.marginLeft = toc.offsetWidth + 'px';
     };
 
     // TOC toggle button
-    var bodyOffset = function() {
+    var option = {
+      duration: config.button.duration,
+      step: function() {
         body.style.marginLeft = toc.offsetWidth + 'px';
-    }
+      }
+    };
     $(button).click(function() {
         if (toc.offsetWidth > 0) {
             $(button).css({
                 background: config.button.color.active,
                 transform: 'rotate(-45deg)'
             });
-            $(toc).animate({ width: '0', minWidth: '0' }, {
-                duration: config.button.duration,
-                step: bodyOffset
-            });
+            $(toc).animate({ width: '0', minWidth: '0' }, option);
         }
         else {
             $(button).css({
                 background: config.button.color.bg,
                 transform: 'rotate(0)'
             });
-            $(toc).animate({ width: config.toc.width, minWidth: config.toc.minwidth }, {
-                duration: config.button.duration,
-                step: bodyOffset
-            });
+            $(toc).animate({ width: config.toc.width, minWidth: config.toc.minwidth }, option);
         }
     });
 }, false);
