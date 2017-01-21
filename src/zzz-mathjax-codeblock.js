921588d06c5c1c8b899fd312dd99c78d8931e68c
nocd5 <nocd5rd@gmail.com>
2017-01-21 23:17:27 +0900

    Remove debug log

--- src/zzz-mathjax-codeblock.js
+++ src/zzz-mathjax-codeblock.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     for (i = lngmath.length-1; i >= 0; i--) {
         if (lngmath[i].tagName == 'PRE') {
             var code = lngmath[i].getElementsByClassName('language-math');
-            console.log(code)
             var mj = document.createElement('p');
             mj.innerHTML = '$$'
             for (j = 0; j < code.length; j++) {
-              console.log(code[j].innerHTML)
                 mj.innerHTML += code[j].innerHTML
             }
             mj.innerHTML += '$$'
