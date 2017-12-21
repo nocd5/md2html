3988ffdf3fe37cec27d09326a5a735af4e9d7b21
nocd5 <nocd5rd@gmail.com>
2017-12-21 18:10:04 +0900

    Refactoring

--- src/zzz-post.js
+++ src/zzz-post.js
@@ -76,83 +76,82 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // colspan
+    // translate column
     var recol = new RegExp('\u00a6\\s*', 'g');
-    var colspan = function(cells) {
-        var cellary = Array.prototype.slice.call(cells, 0)
-        var ts = 0;
-        cellary.forEach(function(cell, _) {
-            var seps = cell.innerHTML.match(recol)
-            if (seps != null && seps.length > 0) {
-                cell.innerHTML = cell.innerHTML.replace(recol, '');
-                ts += seps.length;
-                cell.setAttribute('colspan', 1 + seps.length);
+    var trans_col = function(rows, elm) {
+        [].forEach.call(rows, function(row) {
+            var cells = row.querySelectorAll(elm);
+            var ts = 0;
+            Array.prototype.forEach.call(cells, function(cell) {
+                var seps = cell.innerHTML.match(recol)
+                if (seps != null && seps.length > 0) {
+                    cell.innerHTML = cell.innerHTML.replace(recol, '');
+                    ts += seps.length;
+                    cell.setAttribute('colspan', 1 + seps.length);
+                }
+            });
+            for (var i = 1; i <= ts; i++) {
+                cells[cells.length-i].outerHTML = '';
             }
         });
-        for (var j = 1; j <= ts; j++) {
-            cellary[cells.length-j].outerHTML = '';
-        }
-    }
-    // rowspan
-    var colpos = function(row, n) {
+    };
+    // translate row
+    var get_colpos = function(row, n) {
         var pos = 0;
         var i = 0;
         while (true) {
-            var s = row[i].getAttribute('colspan');
-            pos += s == null ? 1 : Number(s);
-            if (pos > n) {
+            var s = row[pos].getAttribute('colspan');
+            i += s == null ? 1 : Number(s);
+            if (i > n) {
                 break;
             }
-            i++;
+            pos++;
         }
-        return i;
-    }
-    var rowspan = function(rows, elm) {
-        var cells = [];
+        return pos;
+    };
+    var trans_row = function(rows, elm) {
+        var datalist = [];
         var maxcol = 0;
-        for (var i = 0; i < rows.length; i++) {
-            cells.push(Array.prototype.slice.call(rows[i].querySelectorAll(elm), 0));
+        [].forEach.call(rows, function(row) {
+            var cells = row.querySelectorAll(elm);
+            datalist.push(cells);
             var col = 0;
-            cells[i].forEach(function(cell) {
+            [].forEach.call(cells, function(cell) {
                 var s = cell.getAttribute('colspan');
                 col += s == null ? 1 : Number(s);
             });
             maxcol = Math.max(maxcol, col);
-        }
+            i++;
+        });
         for (var i = 0; i < maxcol; i++) {
             var span = 0;
-            var root = colpos(cells[0], i);
-            for (var j = 1; j < cells.length; j++) {
-                var cur = colpos(cells[j], i);
-                if (cells[j][cur] != null && cells[j][cur].innerHTML == '') {
+            var root = get_colpos(datalist[0], i);
+            for (var j = 1; j < datalist.length; j++) {
+                var cur = get_colpos(datalist[j], i);
+                if (datalist[j][cur] != null && datalist[j][cur].innerHTML == '') {
                     span++;
-                    if (cells[j - span] != null && cells[j - span][root] != null) {
-                        cells[j - span][root].setAttribute('rowspan', 1 + span);
-                        if (cells[j][cur].parentNode != null) {
-                            cells[j][cur].outerHTML = '';
+                    if (datalist[j - span] != null && datalist[j - span][root] != null) {
+                        datalist[j - span][root].setAttribute('rowspan', 1 + span);
+                        if (datalist[j][cur].parentNode != null) {
+                            datalist[j][cur].outerHTML = '';
                         }
                     }
                 }
                 else {
-                    root = colpos(cells[j], i);
+                    root = get_colpos(datalist[j], i);
                     span = 0;
                 }
             }
         }
-    }
-    var tbls = document.querySelectorAll('table');
-    Array.prototype.slice.call(tbls, 0).forEach(function(tbl, _) {
+    };
+    [].forEach.call(document.querySelectorAll('table'), function(tbl) {
         // thead
         var headrows = tbl.querySelectorAll('thead > tr');
-        Array.prototype.forEach.call(headrows, function(row, _) {
-            colspan(row.querySelectorAll('th'));
-        });
-        rowspan(headrows, 'th');
+        trans_col(headrows, 'th');
+        trans_row(headrows, 'th');
         // tbody
         var rows = tbl.querySelectorAll('tbody > tr');
-        Array.prototype.forEach.call(rows, function(row, _) {
-            colspan(row.querySelectorAll('td'));
-        });
-        rowspan(rows, 'td');
+        trans_col(rows, 'td');
+        trans_row(rows, 'td');
     });
 }, false);
