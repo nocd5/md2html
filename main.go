1f451087a6d81aa9f14eb129e903a452ce2e503e
nocd5 <nocd5rd@gmail.com>
2020-12-26 11:53:11 +0900

    Fix clash if output filename contains meta character

--- main.go
+++ main.go
@@ -118,7 +118,8 @@ func main() {
 	)
 
 	if len(opts.OutputFile) > 0 {
-		re := regexp.MustCompile(filepath.Ext(opts.OutputFile) + "$")
+		ext := regexp.QuoteMeta(filepath.Ext(opts.OutputFile))
+		re := regexp.MustCompile(ext + "$")
 		title := filepath.Base(re.ReplaceAllString(opts.OutputFile, ""))
 		html, err := renderHtmlConcat(files, opts.EmbedImage, parser)
 		if err != nil {
