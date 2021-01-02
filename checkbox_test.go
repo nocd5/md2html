2f41ebf8c99530d8273159796064d0d0b3829ab8
nocd5 <nocd5rd@gmail.com>
2021-01-02 18:41:01 +0900

    Update test

--- checkbox_test.go
+++ checkbox_test.go
@@ -1,27 +1,52 @@
 package main
 
 import (
+	"github.com/PuerkitoBio/goquery"
+	"strings"
 	"testing"
 )
 
 func TestReplaceCheckBox0(t *testing.T) {
-	result, err := replaceCheckBox("<li>[ ]ab</li>")
+	result, err := replaceCheckBox("<li><input disabled=\"\" type=\"checkbox\"/> ab</li>")
 	if err != nil {
 		t.Fatalf("Failed Test %#v", err)
 	}
-	if result != "<li class=\"task-list-item\"><input disabled=\"\" type=\"checkbox\"/> ab</li>" {
+	r, err := checkClass(result)
+	if err != nil {
+		t.Fatalf("Failed Test %#v", err)
+	}
+	if !r {
 		t.Fatalf("Failed Test %#v", result)
 	}
 }
 
 func TestReplaceCheckBox1(t *testing.T) {
-	result, err := replaceCheckBox("<li>[ ]</li>")
+	result, err := replaceCheckBox("<li><input disabled=\"\" type=\"checkbox\"/> </li>")
 	if err != nil {
 		t.Fatalf("Failed Test %#v", err)
 	}
-	if result != "<li class=\"task-list-item\"><input disabled=\"\" type=\"checkbox\"/> </li>" {
+	r, err := checkClass(result)
+	if err != nil {
+		t.Fatalf("Failed Test %#v", err)
+	}
+	if !r {
 		t.Fatalf("Failed Test %#v", result)
 	}
+}
+
+func checkClass(node string) (bool, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(node))
+	if err != nil {
+		return false, err
+	}
+	lis := doc.Find("li")
+	if lis.Length() != 1 {
+		return false, nil
+	}
+	if !lis.First().HasClass("task-list-item") {
+		return false, nil
+	}
+	return true, nil
 }
 
 func TestReplaceCheckBox2(t *testing.T) {
